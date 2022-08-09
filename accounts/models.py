37306b4fd12f8cfe53a47cc33c67c7789270425e
irvingfmddev <irvingfabianmartinezdiaz@outlook.com>
2022-08-09 01:19:21 -0500

    Primer commit del proyecto

---
+++ accounts/models.py
@@ -0,0 +1,72 @@
+from enum import Flag
+from multiprocessing.sharedctypes import Value
+from pyexpat import model
+from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager)
+
+# Create your models here.
+class MyAccountManager(BaseUserManager):
+    # Create user
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError('El usuario debe tener un email')
+
+        if not username:
+            raise ValueError('El usuario debe tener un usuario')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    # Create superuser
+    def create_superuser(self, first_name, last_name, email, username, password=None):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+        )
+
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.CharField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=50)
+
+    # Fields attributes of django
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    objects = MyAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
