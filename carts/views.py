361b438bfca203b773516bc91e3281cb2e03fcbb
irvingfmddev <irvingfabianmartinezdiaz@outlook.com>
2022-08-12 03:54:08 -0500

    Funcionalidad y matenimiento al carrito

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,10 @@
+from gc import get_objects
+from itertools import product
 from logging import exception
 from math import prod
+from django.http import HttpResponse
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
@@ -20,30 +23,54 @@ def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
-    
+
     except Cart.DoesNotExist:
         cart = Cart.objects.create(
             cart_id = _cart_id(request)
         )
     cart.save()
 
-# Agregar el item al carrito de compra que se va a comprar
-    try: 
+    try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
-        cart_item.quantity +=1
-
+        cart_item.quantity += 1
         cart_item.save()
-    
+
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(
             product = product,
             quantity = 1,
             cart = cart,
         )
+    cart_item.save()
 
+    return redirect('cart')
+
+# Eliminar items del carrito
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        
         cart_item.save()
 
-        return redirect('cart')
+    else:
+        cart_item.delete()
+
+    return redirect('cart')
+
+
+# Eliminar por completo el item del carrito
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+
+    return redirect('cart')
+
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
