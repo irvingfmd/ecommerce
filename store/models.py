2f5c6a82b0ddcb685b045a397e70b651daf43efb
irvingfmddev <irvingfabianmartinezdiaz@outlook.com>
2022-08-15 03:03:45 -0500

    Variations en carrito terminado

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from audioop import reverse
 from distutils.command.upload import upload
+from itertools import product
 from tkinter import CASCADE
 from unicodedata import category
 
@@ -25,3 +26,27 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+    
+    def tallas(self):
+        return super(VariationManager, self).filter(variation_category='talla', is_active=True)
+
+variation_category_choices = (
+    ('color', 'color'),
+    ('talla', 'talla'),
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choices)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_category + ':' + self.variation_value
