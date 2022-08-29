06806d9453bdb3edfab019094132bb285bf53939
irvingfmddev <irvingfabianmartinezdiaz@outlook.com>
2022-08-28 23:17:59 -0500

    Pagos con paypal listo

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,106 @@
-from django.shortcuts import render
+from contextlib import redirect_stderr
+from datetime import datetime
+from multiprocessing import context
+
+from django.shortcuts import render, redirect
+
+from carts.models import CartItem
+from .forms import OrderForm
+from .models import Order
+from .models import Order, Payment
+
+import datetime
+import json
 
 # Create your views here.
+def payments(request):
+    body = json.loads(request.body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['orderID'])
+
+    payment = Payment(
+        user = request.user,
+        payment_id = body['transID'],
+        payment_method = body['payment_method'],
+        amount_id = order.order_total,
+        status = body['status'],
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    
+    order.save()
+
+    return render(request, 'orders/payments.html')
+
+def place_order(request, total=0, quantity=0):
+    current_user = request.user
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+
+    if cart_count <= 0:
+        return redirect('store')
+
+    pre_tax = 0
+    tax = 0
+    pre_grand_total = 0
+    grand_total = 0
+
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+    
+    pre_tax = (16 * total) / 100
+    tax = round(pre_tax, 2)
+    pre_grand_total = total + tax
+    grand_total = round(pre_grand_total, 2)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+
+        if form.is_valid():
+            data = Order()
+            data.user = current_user
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.phone = form.cleaned_data['phone']
+            data.email = form.cleaned_data['email']
+            data.address_line_1 = form.cleaned_data['address_line_1']
+            data.address_line_2 = form.cleaned_data['address_line_2']
+            data.country = form.cleaned_data['country']
+            data.state = form.cleaned_data['state']
+            data.city = form.cleaned_data['city']
+            data.order_note = form.cleaned_data['order_note']
+            data.order_total = pre_grand_total
+            data.tax = pre_tax
+            data.ip = request.META.get('REMOTE_ADDR')
+            
+            data.save()
+
+            yr = int(datetime.date.today().strftime('%Y'))
+            mt = int(datetime.date.today().strftime('%m'))
+            dt = int(datetime.date.today().strftime('%d'))
+
+            d = datetime.date(yr, mt, dt)
+            current_date = d.strftime("%Y%m%d")
+            order_number = current_date + str(data.id)
+            data.order_number = order_number
+
+            data.save()
+
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'pre_tax': pre_tax,
+                'tax': tax,
+                'pre_grand_total': pre_grand_total,
+                'grand_total': grand_total,
+            }
+
+            return render(request, 'orders/payments.html', context)
+        
+        else:
+            return redirect('checkout')
